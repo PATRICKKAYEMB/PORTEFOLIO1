7671471b97bdd64039ee16e7076b77a641a9b3e2
patrickkayemb <patrickkayemba81@gmail.com>
2025-02-19 12:37:08 +0100

    version1 modification du composant about

--- src/Conposants/About.jsx
+++ src/Conposants/About.jsx
@@ -7,40 +7,57 @@ const About = () => {
 
   return (
     <div id="about" className="container mx-auto px-4 py-16 bg-white">
-      <div ref={ref} className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
       
-        <div
-          
-          className="space-y-4"
-        >
-          <h2 className="text-2xl md:text-4xl font-bold text-indigo-600">A Propos de Moi</h2>
-          <p className="text-gray-600">
-            Freelance en développement web, je transforme vos idées en sites professionnels et interfaces intuitives. 
-            Avec une passion pour le design et une expertise technique, je crée des expériences visuelles et fonctionnelles captivantes. 
-            Mon approche collaborative garantit un projet fidèle à votre vision tout en répondant aux attentes de votre audience.
-            Ensemble, faisons de votre présence en ligne un véritable atout pour votre succès.
-          </p>
+      <div class="space-y-4" >
+                    <h2 class="text-2xl md:text-4xl font-bold text-indigo-600 ">A Propos de Moi</h2>
+                    <p class="text-gray-600">Freelance en développement web, je transforme vos idées en sites professionnels et interfaces intuitives. Avec une passion pour le design et une expertise technique, je crée des expériences visuelles et fonctionnelles captivantes. Mon approche collaborative garantit un projet fidèle à votre vision tout en répondant aux attentes de votre audience.
+                         Ensemble, faisons de votre présence en ligne un véritable atout pour votre succès.</p>
+                    
+                  
+                    <div class="space-y-4">
+                        <div>
+                            <div class="flex justify-between mb-1">
+                                <span class="text-gray-700">HTML</span>
+                                <span class="text-gray-700">90%</span>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2">
+                                <div class="bg-indigo-600 h-2 rounded-full" style={{width:"90%"}}></div>
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <div class="flex justify-between mb-1">
+                                <span class="text-gray-700">CSS</span>
+                                <span class="text-gray-700">85%</span>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2">
+                                <div class="bg-indigo-600 h-2 rounded-full" style={{width:"90%"}}></div>
+                            </div>
+                        </div>
 
-         
-          <div className="space-y-4">
-            {[
-              { name: "HTML", value: "90%" },
-              { name: "CSS", value: "85%" },
-              { name: "Tailwind CSS", value: "80%" },
-              { name: "React", value: "70%" },
-            ].map((skill, index) => (
-              <div key={index}>
-                <div className="flex justify-between mb-1">
-                  <span className="text-gray-700">{skill.name}</span>
-                  <span className="text-gray-700">{skill.value}</span>
+                        <div>
+                            <div class="flex justify-between mb-1">
+                                <span class="text-gray-700">Tailwind CSS</span>
+                                <span class="text-gray-700">80%</span>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2">
+                                <div class="bg-indigo-600 h-2 rounded-full" ></div>
+                                <div class="bg-indigo-600 h-2 rounded-full"  style={{width:"80%"}}></div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="flex justify-between mb-1">
+                                <span class="text-gray-700">React</span>
+                                <span class="text-gray-700">70%</span>
+                            </div>
+                            <div class="w-full bg-gray-200 rounded-full h-2">
+                                <div class="bg-indigo-600 h-2 rounded-full"  style={{width:"70%"}}></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full bg-gray-200 rounded-full h-2">
-                  <div className="bg-indigo-600 h-2 rounded-full" style={{ width: skill.value }}></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
 
               
         <div className="relative" >
